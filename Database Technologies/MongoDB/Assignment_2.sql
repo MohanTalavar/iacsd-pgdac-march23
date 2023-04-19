037e2f5433f7815cb876734a87a2c7e368fd34c0
Mohan Malappa Talavar <123090503+MohanTalavar@users.noreply.github.com>
2023-04-19 23:29:43 +0530

    Create Assignment_2.sql
    Signed-off-by: Mohan Malappa Talavar <123090503+MohanTalavar@users.noreply.github.com>

---
+++ Database Technologies/MongoDB/Assignment_2.sql
@@ -0,0 +1,27 @@
+Create a Employee Collection add 5 documents:
+Example: 
+{empno:111,ename:”Deepali 
+Vaidya”,sal:40000.00,dept:{deptno12,dname:,”Hr”,dloc:”Mumbai},
+Desg:”Analyst”,mgr:{name:”Satish”,num:111},project:[{name:”Project-1”,Hrs:4},{name:”project- 2”,Hrs:4}]}
+>
+db.emp.insertMany([
+  {
+    empno: 111,
+    ename: "Deepali Vaidya",
+    sal: 40000.00,
+    dept: {
+      deptno: 12,
+      dname: "Hr",
+      dloc: "Mumbai"
+    },
+    Desg: "Analyst",
+    mgr: {
+      name: "Satish",
+      num: 111
+    },
+    project: [
+      { name: "Project-1", Hrs: 4 },
+      { name: "project-2", Hrs: 4 }
+    ]
+  }
+]);
