9969c10711d07ecd5cc290bfb9e2816f730e6c61
Mohan Malappa Talavar <123090503+MohanTalavar@users.noreply.github.com>
2023-04-22 23:35:39 +0530

    Update Assignment_2.sql
    Signed-off-by: Mohan Malappa Talavar <123090503+MohanTalavar@users.noreply.github.com>

--- Database Technologies/MongoDB/Assignment_2.sql
+++ Database Technologies/MongoDB/Assignment_2.sql
@@ -1,3 +1,4 @@
+Assignment Pre-requisit:
 Create a Employee Collection add 5 documents:
 Example: 
 {empno:111,ename:”Deepali 
@@ -26,3 +27,42 @@ db.emp.insertMany([
 	bonus: 10000
   }
 ]);
+
+Assignment 2 Questions:
+
+1. All Employee’s with the desg as ‘CLERK’ are now called as (AO) Administrative Officers. Update the Employee collection for this.
+> 
+db.emp.update({Desg:'Clerk'},
+{$set: {Desg:'AO'}},
+{multi:true});
+
+2. Change the number of hours for project-1 to 5 for all employees with designation analyst.
+>
+db.emp.update({ "project.name": 'Project-1', Desg: 'Analyst'},
+{$set:{ "project.$.Hrs": 5}},
+{mult:true})
+
+
+3. Add 2 projects project-3 and project-4 for employee whose 
+name starts with ”Deep” with 2 hrs
+> 
+db.emp.update( {ename: /^Deep/}, 
+{$push: { project: {$each: [{name:'Project-3',Hrs:2},
+			    {name:'Project-4',Hrs:2}
+]}} },
+{multi:true}
+)
+
+4. Add bonus rs 2000 for all employees with salary > 50000 
+>
+db.emp.update({sal: {$gt:50000}  },
+ {$inc:{sal:2000 }},
+ {multi:true}
+ )
+
+5. Add bonus rs 1500 if salary <50000 and > 30000 
+>
+db.emp.update( { sal:{$gt:30000, $lt:50000 }},
+ { $inc: {sal:15000}},
+ {multi:true}
+ )
